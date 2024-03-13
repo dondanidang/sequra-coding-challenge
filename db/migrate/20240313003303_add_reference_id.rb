0a39fec7a494a6b926e6b26bb53c90a0be4fc084
Daniel Dang <dondanidang@gmail.com>
2024-03-13 14:54:07 +0100

    Wip

--- db/migrate/20240313003303_add_reference_id.rb
+++ db/migrate/20240313003303_add_reference_id.rb
@@ -1,6 +1,6 @@
 class AddReferenceId < ActiveRecord::Migration[7.1]
   def change
-    add_column :orders, :external_reference_id, :string, null: false
+    add_column :orders, :external_reference_id, :string
     add_index :orders, :external_reference_id, unique: true
   end
 end
