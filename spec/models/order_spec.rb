30e739965f18f12282c310ed778443f125d193d1
Daniel Dang <dondanidang@gmail.com>
2024-03-13 16:36:14 +0100

    Wip

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  it { is_expected.to belong_to(:merchant) }
+  it { is_expected.to belong_to(:disbursement) }
+end
