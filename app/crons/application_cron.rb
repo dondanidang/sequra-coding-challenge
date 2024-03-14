21de9a10c733dba8147c94c1ea325c0205b0d628
Daniel Dang <dondanidang@gmail.com>
2024-03-14 20:58:51 +0100

    Wip

--- app/crons/application_cron.rb
+++ app/crons/application_cron.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-class ApplicationService
-  def self.run(*args, **kwargs)
-    new(*args, **kwargs).send(:run)
+module Crons
+  class ApplicationService
+    def self.run(*args, **kwargs)
+      new(*args, **kwargs).send(:run)
+    end
   end
 end
